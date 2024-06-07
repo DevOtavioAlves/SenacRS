a32ecb56e554984504bbe1388988678cdc8ff7e0
Sh00TingB0y <tata.guimaraes07@gmail.com>
2024-06-07 20:44:29 -0300

    listagemVIEW com botão da tela de vendasVIEW funcionando

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -160,8 +160,8 @@ public class listagemVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
-         //vendasVIEW vendas = new vendasVIEW(); 
-         //vendas.setVisible(true);
+         vendasVIEW vendas = new vendasVIEW(); 
+         vendas.setVisible(true);
     }//GEN-LAST:event_btnVendasActionPerformed
 
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
